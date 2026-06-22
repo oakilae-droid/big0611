8c930cb9113582c70269603dc36733ae4cf7c116
oakilae-droid <oakilae@gmail.com>
2026-06-22 17:51:28 +0900

    6/22 마무리

--- py/p01/basic/ch06/ex01_시스템입력.py
+++ py/p01/basic/ch06/ex01_시스템입력.py
@@ -1,6 +1,7 @@
 # 시스템 입력
 # inout()
-print(input('입력:'))
+print('메시지')
+input('입력 메시지 기록')
 
 # 사용자로부터 이름을 입력 
 '''
@@ -8,4 +9,18 @@ print(input('입력:'))
 '''
 
 name = input('이름을 입력하세요:')
-print(name)
+print(name + '님, 안녕하세요')
+
+# 키에 따른 권장체중
+# input 항상 문자열을 반환한다. -> int('184') -> 184
+height = input('키를 입력하세요: ')
+print('나의 키는 ' + height + 'cm 입니다.')
+height = float(height)
+weight = (height - 100) * 0.9
+# TypeError: unsupported operand type(s) for -: 'str' and 'int'
+# print(당신의 권장 체중은 weight 입니다.)
+print('당신의 권장체중은 ' + str(weight) + 'kg 입니다.')
+# TypeError: can only concatenate str (not "float") to str
+print('당신의 권장체중은 {}kg 입니다.'.format(weight))
+# f-string
+print(f'당신의 권장체중은 {weight}kg 입니다.')
---
+++ py/p01/basic/ch06/ex06_파일입출력.py
@@ -0,0 +1,49 @@
+# 파일 입력: 파일 내용 쓰기
+# 파일 모드
+'''
+r(read): 읽기 -> read()
+w(write): 쓰기 -> write(), 덮어 씌운다.
+a(append): 추가 -> write()
+'''
+f = open('abc1.txt', 'w')
+f.write('A B C D E F G ')
+f.close()
+
+f = open('abc1.txt', 'w')
+f.write('a b c d e f g ')
+f.close()
+
+f = open('abc1.txt', 'a')
+f.write('H I J K L M N O P Q R S T U V W X Y Z')
+f.close()
+
+
+# 파일 출력: 파일 내용 읽기
+# read(): 글자 1자씩 읽기
+f = open('abc1.txt', 'r')
+print(f.read())
+f.close()
+
+# readlines(): 글자 한 줄씩 읽기
+f = open('abc2.txt', 'r')
+lines = f.readlines()
+for line in lines:
+    print(line)
+f.close()
+
+
+# with 문: close() 자동 처리
+'''
+with open('파일명', '파일모드', encoding='utf-8') as 별칭(변수):
+    별칭.write()
+    별칭.read()
+'''
+# 윈도우 기본값: encoding='cp949'
+with open('일기.txt', 'w', encoding='utf8') as f:
+    f.write('2026년 6월 22일 월요일\n')
+
+with open('일기.txt', 'a', encoding='utf-8') as f:
+    f.write('대체로 흐림')
+
+with open('일기.txt', 'r', encoding='utf-8') as f:
+    print(f.read())
