e3d4e7a87e62013319b01f8c49959f0291e2c483
oakilae-droid <oakilae@gmail.com>
2026-08-04 12:18:41 +0900

    진도 보충

---
+++ py/p03_크롤링/ch01_beautifulsoup4/ex01_bs4.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+html_doc = '''
+<!doctype html>
+<html>
+    <head>
+        <title>기초 웹 크롤링</title>
+    </head>
+    <body>
+        크롤링을 해봅시다.
+    </body>
+</html>
+'''
+
+# BeautifulSoup(텍스트, '파서종류')
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup)
